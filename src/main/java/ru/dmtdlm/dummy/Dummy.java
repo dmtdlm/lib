113435507a140237b4be14051e3e0d89b8ad4a01
dmtdlm <dmtdlm@yandex.ru>
2017-02-20 00:07:51 +0300

    first

---
+++ src/main/java/ru/dmtdlm/dummy/Dummy.java
@@ -0,0 +1,12 @@
+package ru.dmtdlm.dummy;
+
+/**
+ * Created by Dmitry on 19.02.2017.
+ */
+public class Dummy
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/test/java/emptyTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Dmitry on 19.02.2017.
+ */
+public class emptyTest
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
